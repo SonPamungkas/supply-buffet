8974898ac4c2cf8dbcb3cc60a9afade561373251
SonPamungkas <nononon928@gmail.com>
2026-07-03 20:52:08 +0700

    Add replenishment and cooldown configurations for units

--- Plugin.cs
+++ Plugin.cs
@@ -24,6 +24,20 @@ namespace SupplyBuffetMod
         public static ConfigEntry<float> MunitionsContainerRadius;
         public static ConfigEntry<float> NavalContainerRadius;
 
+        public static ConfigEntry<bool> MunitionsPalletReplenishable;
+        public static ConfigEntry<bool> NavalPalletReplenishable;
+        public static ConfigEntry<bool> MunitionsContainerReplenishable;
+        public static ConfigEntry<bool> NavalContainerReplenishable;
+
+        public static ConfigEntry<float> MunitionsPalletCheckInterval;
+        public static ConfigEntry<float> NavalPalletCheckInterval;
+        public static ConfigEntry<float> MunitionsContainerCheckInterval;
+        public static ConfigEntry<float> NavalContainerCheckInterval;
+
+        public static ConfigEntry<float> UnitCooldown;
+
+        public static ConditionalWeakTable<Unit, StrongBox<float>> UnitLastRearmTime = new ConditionalWeakTable<Unit, StrongBox<float>>();
+
         public static readonly Dictionary<string, string> AdaptiveSupplyPairs = new Dictionary<string, string>
         {
             { "MunitionsPallet1", "NavalPallet1" },
@@ -62,7 +76,19 @@ namespace SupplyBuffetMod
             MunitionsContainerRadius = Config.Bind("SupplyRadius", "MunitionsContainer1", 1500f, "Supply radius for Munitions Container");
             NavalContainerRadius = Config.Bind("SupplyRadius", "NavalSupplyContainer1", 1500f, "Supply radius for Naval Container");
 
+            MunitionsPalletReplenishable = Config.Bind("SupplyContainer", "MunitionsPallet1_Replenishable", true, "Whether the Munitions Pallet can be used multiple times.");
+            NavalPalletReplenishable = Config.Bind("SupplyContainer", "NavalPallet1_Replenishable", true, "Whether the Naval Pallet can be used multiple times.");
+            MunitionsContainerReplenishable = Config.Bind("SupplyContainer", "MunitionsContainer1_Replenishable", true, "Whether the Munitions Container can be used multiple times.");
+            NavalContainerReplenishable = Config.Bind("SupplyContainer", "NavalSupplyContainer1_Replenishable", true, "Whether the Naval Container can be used multiple times.");
+
+            MunitionsPalletCheckInterval = Config.Bind("SupplyContainer", "MunitionsPallet1_CheckInterval", 1f, "Throttle (in seconds) between resupply checks for the Munitions Pallet.");
+            NavalPalletCheckInterval = Config.Bind("SupplyContainer", "NavalPallet1_CheckInterval", 1f, "Throttle (in seconds) between resupply checks for the Naval Pallet.");
+            MunitionsContainerCheckInterval = Config.Bind("SupplyContainer", "MunitionsContainer1_CheckInterval", 1f, "Throttle (in seconds) between resupply checks for the Munitions Container.");
+            NavalContainerCheckInterval = Config.Bind("SupplyContainer", "NavalSupplyContainer1_CheckInterval", 1f, "Throttle (in seconds) between resupply checks for the Naval Container.");
+
             AutoRequestRearmEnabled = Config.Bind("AutoRequestRearm", "Enabled", true, "Let ships and ground vehicles automatically request rearm (join the resupply demand queue) after firing leaves a weapon station short of ammo, instead of requiring a manual player request.");
+
+            UnitCooldown = Config.Bind("Rearming", "UnitCooldown", 10f, "Minimum time (in seconds) between successive resupplies of the same unit, to prevent nonstop firing/rearm loops.");
 
             Harmony harmony = new Harmony("com.neutral.supplybuffet");
             harmony.PatchAll();
@@ -157,27 +183,120 @@ namespace SupplyBuffetMod
                 if (__instance.gameObject == null) return;
                 string name = __instance.gameObject.name;
 
+                Traverse obj = Traverse.Create(__instance);
+
+                float range;
                 if (name.Contains("MunitionsPallet1"))
                 {
-                    Traverse.Create(__instance).Field("range").SetValue(Plugin.MunitionsPalletRadius.Value);
+                    range = Plugin.MunitionsPalletRadius.Value;
+                    obj.Field("range").SetValue(range);
+                    obj.Field("singleUse").SetValue(!Plugin.MunitionsPalletReplenishable.Value);
+                    obj.Field("checkInterval").SetValue(Plugin.MunitionsPalletCheckInterval.Value);
                 }
                 else if (name.Contains("NavalPallet1"))
                 {
-                    Traverse.Create(__instance).Field("range").SetValue(Plugin.NavalPalletRadius.Value);
+                    range = Plugin.NavalPalletRadius.Value;
+                    obj.Field("range").SetValue(range);
+                    obj.Field("singleUse").SetValue(!Plugin.NavalPalletReplenishable.Value);
+                    obj.Field("checkInterval").SetValue(Plugin.NavalPalletCheckInterval.Value);
                 }
                 else if (name.Contains("MunitionsContainer1"))
                 {
-                    Traverse.Create(__instance).Field("range").SetValue(Plugin.MunitionsContainerRadius.Value);
+                    range = Plugin.MunitionsContainerRadius.Value;
+                    obj.Field("range").SetValue(range);
+                    obj.Field("singleUse").SetValue(!Plugin.MunitionsContainerReplenishable.Value);
+                    obj.Field("checkInterval").SetValue(Plugin.MunitionsContainerCheckInterval.Value);
                 }
                 else if (name.Contains("NavalSupplyContainer1"))
                 {
-                    Traverse.Create(__instance).Field("range").SetValue(Plugin.NavalContainerRadius.Value);
+                    range = Plugin.NavalContainerRadius.Value;
+                    obj.Field("range").SetValue(range);
+                    obj.Field("singleUse").SetValue(!Plugin.NavalContainerReplenishable.Value);
+                    obj.Field("checkInterval").SetValue(Plugin.NavalContainerCheckInterval.Value);
                 }
+                else
+                {
+                    return;
+                }
+
+                Plugin.Log.LogInfo($"[SupplyBuffetMod] Configured Rearmer '{name}': range={range}.");
             }
             catch (Exception ex)
             {
                 Plugin.Log.LogError($"[SupplyBuffetMod] Error in Rearmer_Start_SupplyRadius_Patch: {ex}");
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(Ship), "Rearm", new Type[] { typeof(RearmEventArgs) })]
+    public class Ship_Rearm_Timer_Patch
+    {
+        static void Postfix(Ship __instance)
+        {
+            Plugin.UnitLastRearmTime.GetOrCreateValue(__instance).Value = Time.timeSinceLevelLoad;
+        }
+    }
+
+    [HarmonyPatch(typeof(GroundVehicle), "Rearm", new Type[] { typeof(RearmEventArgs) })]
+    public class GroundVehicle_Rearm_Timer_Patch
+    {
+        static void Postfix(GroundVehicle __instance)
+        {
+            Plugin.UnitLastRearmTime.GetOrCreateValue(__instance).Value = Time.timeSinceLevelLoad;
+        }
+    }
+
+    [HarmonyPatch(typeof(Aircraft), "Rearm", new Type[] { typeof(RearmEventArgs) })]
+    public class Aircraft_Rearm_Timer_Patch
+    {
+        static void Postfix(Aircraft __instance)
+        {
+            Plugin.UnitLastRearmTime.GetOrCreateValue(__instance).Value = Time.timeSinceLevelLoad;
+        }
+    }
+
+    [HarmonyPatch(typeof(Ship), "CanRearm", new Type[] { typeof(bool), typeof(bool), typeof(bool) })]
+    public class Ship_CanRearm_Cooldown_Patch
+    {
+        static bool Prefix(Ship __instance, ref bool __result)
+        {
+            if (Plugin.UnitLastRearmTime.TryGetValue(__instance, out var box)
+                && Time.timeSinceLevelLoad - box.Value < Plugin.UnitCooldown.Value)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(GroundVehicle), "CanRearm", new Type[] { typeof(bool), typeof(bool), typeof(bool) })]
+    public class GroundVehicle_CanRearm_Cooldown_Patch
+    {
+        static bool Prefix(GroundVehicle __instance, ref bool __result)
+        {
+            if (Plugin.UnitLastRearmTime.TryGetValue(__instance, out var box)
+                && Time.timeSinceLevelLoad - box.Value < Plugin.UnitCooldown.Value)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Aircraft), "CanRearm", new Type[] { typeof(bool), typeof(bool), typeof(bool) })]
+    public class Aircraft_CanRearm_Cooldown_Patch
+    {
+        static bool Prefix(Aircraft __instance, ref bool __result)
+        {
+            if (Plugin.UnitLastRearmTime.TryGetValue(__instance, out var box)
+                && Time.timeSinceLevelLoad - box.Value < Plugin.UnitCooldown.Value)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
         }
     }
 }
